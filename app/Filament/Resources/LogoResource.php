5e6684091f45da32507242f7d8565b18de26d3ea
fernando <fernando@juandemariana.org>
2025-04-12 23:38:53 +0200

    Add Logo Filament resource

---
+++ app/Filament/Resources/LogoResource.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LogoResource\Pages;
+use App\Filament\Resources\LogoResource\RelationManagers;
+use App\Models\Logo;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class LogoResource extends Resource
+{
+    protected static ?string $model = Logo::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form {
+        return $form->schema([
+            TextInput::make('first_word')->required(),
+            TextInput::make('second_word'),
+            TextInput::make('slogan'),
+        ]);
+    }
+
+    public static function table(Table $table): Table {
+        return $table->columns([
+            TextColumn::make('first_word')->sortable(),
+            TextColumn::make('second_word')->sortable(),
+            TextColumn::make('slogan')->sortable(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLogos::route('/'),
+            'create' => Pages\CreateLogo::route('/create'),
+            'edit' => Pages\EditLogo::route('/{record}/edit'),
+        ];
+    }
+}
