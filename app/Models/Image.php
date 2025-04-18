5f80e02b833802eb4bba4d072eeabec2b693ccfe
fernando <fernando@juandemariana.org>
2025-04-18 12:25:35 +0200

    Relate Post and Image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['path', 'alt'];
+    protected $fillable = ['post_id', 'path', 'alt'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
