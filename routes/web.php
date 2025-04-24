a9380b97e86327147a36fdd33cdaaaa79cd23b5a
fernando <fernando@juandemariana.org>
2025-04-25 00:00:58 +0200

    Add /posts/{post} route

--- routes/web.php
+++ routes/web.php
@@ -54,5 +54,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+
 // Autenticación
 require __DIR__ . '/auth.php';
