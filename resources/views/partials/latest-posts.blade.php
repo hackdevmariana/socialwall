c42fb9567160c3e90204ee4c0e9b1110f521a6ee
fernando <fernando@juandemariana.org>
2025-04-24 23:59:09 +0200

    Add data to latest-post

--- resources/views/partials/latest-posts.blade.php
+++ resources/views/partials/latest-posts.blade.php
@@ -1,3 +1,12 @@
-@foreach(App\Models\Post::latest()->take(5)->get() as $post)
-            <li><a href="#">{{ $post->title }}</a></li>
-        @endforeach
+<ul>
+    @foreach(App\Models\Post::latest()->take(5)->get() as $post)
+        <li>
+            <a href="{{ route('posts.show', $post) }}">
+                <h2>{{ $post->title }}</h2>
+            </a>
+            <img src="{{ asset('storage/' . $post->featured_image) }}" alt="Imagen de {{ $post->title }}" style="max-width: 200px;">
+            <p>Por <strong>{{ $post->user->name }}</strong></p>
+            <p>{{ Str::limit($post->content, 100, '...') }}</p>
+        </li>
+    @endforeach
+</ul>
