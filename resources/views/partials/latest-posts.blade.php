a6d4ea992acf4dac78e70d2fe2599be340bba668
fernando <fernando@juandemariana.org>
2025-04-25 01:16:30 +0200

    Fix latest-post view

--- resources/views/partials/latest-posts.blade.php
+++ resources/views/partials/latest-posts.blade.php
@@ -5,7 +5,8 @@
                 <h2>{{ $post->title }}</h2>
             </a>
             <img src="{{ asset('storage/' . $post->featured_image) }}" alt="Imagen de {{ $post->title }}" style="max-width: 200px;">
-            <p>Por <strong>{{ $post->user->name }}</strong></p>
+            <p>Por <strong>{{ $post->user ? $post->user->name : 'Pobrecito hablador' }}</strong></p>
+
             <p>{{ Str::limit($post->content, 100, '...') }}</p>
         </li>
     @endforeach
